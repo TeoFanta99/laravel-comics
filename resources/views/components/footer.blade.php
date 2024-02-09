91554691691874c7e34fb0c9d88daa6b2298c70d
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-02-09 17:57:44 +0100

    add html

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,12 @@
-<footer>
-    <h1>Hello from footer</h1>
-</footer>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite("resources/js/app.js")
+    <title>Document</title>
+</head>
+<body>
+    <h1>FOOTER</h1>
+</body>
+</html>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,13 @@
 <header>
-    <h1>Hello from Header</h1>
+    <div class="container">
+        <div class="logo-container">
+        <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+        </div>
+        <ul>
+            <li>
+                <a href="">ciao</a>
+            </li>
+        </ul>
+    </div>
 </header>
+
