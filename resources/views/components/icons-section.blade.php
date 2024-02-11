e26a40dc736eef75f3c744d1df32fe1811439ed6
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-02-11 17:56:31 +0100

    fatte icone

---
+++ resources/views/components/icons-section.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite("resources/js/app.js")
+    <title>Document</title>
+</head>
+<body>
+    <div class="icons-section">
+        <div class="container">
+            <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="img1">
+            <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="img">
+            <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="img3">
+            <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="img4">
+            <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="img5">
+        </div>
+    </div>
+</body>
+</html>
