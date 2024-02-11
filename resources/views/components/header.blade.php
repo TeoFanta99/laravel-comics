80a715b09de44ac95f546560f6f4301c62436e4a
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-02-11 15:25:31 +0100

    fatto header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,13 +1,45 @@
 <header>
-    <div class="container">
+    <div class="my_container">
         <div class="logo-container">
-        <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
         </div>
         <ul>
             <li>
-                <a href="">ciao</a>
+                <a href="">CHARACTERS</a>
+            </li>
+            <li>
+                <a href="">COMICS</a>
+            </li>
+            <li>
+                <a href="">MOVIES</a>
+            </li>
+            <li>
+                <a href="">TV</a>
+            </li>
+            <li>
+                <a href="">GAMES</a>
+            </li>
+            <li>
+                <a href="">COLLECTIBLES</a>
+            </li>
+            <li>
+                <a href="">VIDEOS</a>
+            </li>
+            <li>
+                <a href="">FANS</a>
+            </li>
+            <li>
+                <a href="">NEWS</a>
+            </li>
+            <li>
+                <a href="">SHOP</a>
             </li>
         </ul>
+        <div class="searchbar-container">
+            <input type="search" name="" id="" placeholder="Search">
+            <button>CERCA</button>
+        </div>
+        
     </div>
 </header>
 
